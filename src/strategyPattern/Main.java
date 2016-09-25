fdbc41cca2ac2b8699e9f7e4f670ca3528f3d0ed
Brad White <white.brad17@gmail.com>
2016-09-25 13:21:15 -0600

    First Commit

---
+++ src/strategyPattern/Main.java
@@ -0,0 +1,13 @@
+// Sam Congdon & Bradley White
+// ESOF 322
+// HW #2, Strategy Pattern
+// September 25, 2016
+
+package strategyPattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {1,3,4,6,8,9,7,2,5,0};
+    }
+}
